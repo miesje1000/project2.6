0fa194aaa4b36e517f85286305a8e81fac67a0b5
BartBelleman <bartbelleman@hotmail.com>
2016-06-21 09:28:29 +0000

    Add files via upload

--- google custom search API.py
+++ google custom search API.py
@@ -5,7 +5,7 @@ Created on Sun May 29 12:44:56 2016
 @author: Bart
 """
 
-import urllib.request
+import urllib.request, urllib.response
 import urllib
 import json
 
@@ -26,7 +26,7 @@ def telefoonnummerBewerken(nummer):
 
 
 def zoekHuisarts(zoekTerm):
-    #zoekTerm = '0725061263' + ' ' + 'Dhr. F.P. van Geldern'
+    #zoekTerm = '0725061263' + ' ' + 'F.P. van Geldern'
     codeerdeTerm = urllib.parse.quote(zoekTerm, safe='') #de zoekterm wordt veranderd zodat google het kan lezen
     
     APIKey = 'AIzaSyCenzxlkgjkItijd8x0AseTh10hYor-L8I'
@@ -44,3 +44,43 @@ def zoekHuisarts(zoekTerm):
     link = resultaten[0]['link'] #haalt eerste link op uit de resultaen
     
     print(link)
+    
+    
+    
+    
+def zoekHuisarts2():  
+    APIKey = 'AIzaSyC8-TpYB8BnbpdQc1qiCMGtydu40WLseCI'
+    zoekTerm = 'huisarts egmond'
+    query = urllib.parse.quote(zoekTerm, safe='')
+    service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+    params = {
+    'query': query,
+    'limit': 10,
+    'indent': True,
+    'key': APIKey,
+    }
+    
+    url = service_url + '?' + urllib.parse.urlencode(params)
+    url = urllib.request.Request(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"})
+    
+   
+    response = json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
+    print (response)
+    
+    
+def zoekHuisarts3():
+    
+    api_key = 'AIzaSyC8-TpYB8BnbpdQc1qiCMGtydu40WLseCI'
+    query = 'Academisch medisch centrum'
+    service_url = 'https://kgsearch.googleapis.com/v1/entities:search'
+    params = {
+        'query': query,
+        'limit': 10,
+        'indent': True,
+        'key': api_key,
+    }
+    url = service_url + '?' + urllib.parse.urlencode(params)
+    response = json.loads(urllib.request.urlopen(url).read().decode('utf-8'))
+    print(response) 
+
+zoekHuisarts3()
